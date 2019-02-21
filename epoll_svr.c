f7ed82e3e736e378eed9ee21e9ca4a1b493f57a2
Data Comm <dc@bcit.ca>
2019-02-21 15:43:38 -0800

    Uses local buffer instead of global buffer

--- epoll_svr.c
+++ epoll_svr.c
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+static const int MAX_EVENTS = 256;
 //static pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int readAllFromSocket(const int sock, char *buffer, const int size)
@@ -23,6 +24,9 @@ static int readAllFromSocket(const int sock, char *buffer, const int size)
     while (n > 0 && remaining > 0)
     {
         n = recv(sock, bufferPointer, remaining, 0);
+        if (errno == EAGAIN) {
+            break;
+        }
         bufferPointer += n;
         remaining -= n;
     }
@@ -42,6 +46,7 @@ typedef struct {
 	int server_fd;
 	int bufLen;
 	char* buffer;
+	struct epoll_event* events;
 	pthread_mutex_t* mutex;
 } event_loop_args;
 
@@ -53,25 +58,18 @@ bool sendAll(int s, char* buf, int* len);
 
 
 void* eventLoop(void* args) {
-	const int MAX_EVENTS = 256;
 
 	struct epoll_event current_event, event;
 	int n_ready;
 	int status;
 	struct sockaddr_storage remote_addr;
 	socklen_t addr_len;
+	char* local_buffer;
 
 	event_loop_args* ev_args = (event_loop_args*) args;
 
-	int server_fd = ev_args->server_fd;
-	int epoll_fd = ev_args->epoll_fd;
-	const int bufLen = ev_args->bufLen;
-
-	char* buffer = ev_args->buffer;
-	struct epoll_event *events = calloc(MAX_EVENTS, sizeof(struct epoll_event));
-
 	while (true) {
-		n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+		n_ready = epoll_wait(ev_args->epoll_fd, ev_args->events, MAX_EVENTS, -1);
 		if (n_ready == -1) {
 			perror("epoll_wait");
 			exit(EXIT_FAILURE);
@@ -80,7 +78,7 @@ void* eventLoop(void* args) {
 		// A file descriptor is ready
 		for (int i = 0; i < n_ready; ++i) {
 			int client_fd;
-			current_event = events[i];
+			current_event = ev_args->events[i];
 			// An error or hangup occurred
 			// Client might have closed their side of the connection
 			if (current_event.events & (EPOLLHUP | EPOLLERR)) {
@@ -89,15 +87,22 @@ void* eventLoop(void* args) {
 			}
 
 			// The server is receiving a connection request
-			if (current_event.data.fd == server_fd) {
+			if (current_event.data.fd == ev_args->server_fd) {
 				while (true) {
-					client_fd = accept(server_fd, (struct sockaddr*) &remote_addr, &addr_len);
+					client_fd = accept(ev_args->server_fd, (struct sockaddr*) &remote_addr, &addr_len);
 					if (client_fd == -1) {
 						if (errno != EAGAIN && errno != EWOULDBLOCK) {
 							perror("accept");
 						}
 						continue;
 					}
+				    char host[NI_MAXHOST], serv[NI_MAXSERV];
+				    status = getnameinfo((struct sockaddr*) &remote_addr, addr_len, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+				    if (status == -1) {
+					    fprintf(stderr, "getnameinfo: %s\n", gai_strerror(status));
+					    continue;
+				    }
+				    printf("Connection from %s:%s\n", host, serv);
 					// If we get here, then the client_fd is OK
 					break;
 				}
@@ -106,112 +111,70 @@ void* eventLoop(void* args) {
 				// Add the client socket to the epoll instance
 				event.data.fd = client_fd;
 				event.events = EPOLLIN | EPOLLET | EPOLLEXCLUSIVE;
-				status = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
+				status = epoll_ctl(ev_args->epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
 				if (status == -1) {
 					perror("epoll_ctl");
 					exit(EXIT_FAILURE);
 				}
 
-				char host[NI_MAXHOST], serv[NI_MAXSERV];
-				status = getnameinfo((struct sockaddr*) &remote_addr, addr_len, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-				if (status == -1) {
-					fprintf(stderr, "getnameinfo: %s\n", gai_strerror(status));
-					continue;
-				}
-				printf("Connection from %s:%s\n", host, serv);
 				continue;
-			} 
+			}
 			if ((current_event.events & EPOLLIN) == EPOLLIN) {
 				printf("Event on socket %d\n", current_event.data.fd);
-//				pthread_mutex_lock(&g_mutex);
-				if (clearSocket(current_event.data.fd, buffer, bufLen)) {
+				local_buffer = calloc(ev_args->bufLen, sizeof(char));
+				if (clearSocket(current_event.data.fd, local_buffer, ev_args->bufLen)) {
 					printf("Socket %d was cleared\n", current_event.data.fd);
 					//close(current_event.data.fd);
+					free(local_buffer);
 				}
-//				pthread_mutex_unlock(&g_mutex);
 			}
-	//		pthread_mutex_lock(ev_args->mutex);
-			//if (!clearSocket(current_event.data.fd, buffer, bufLen)) { // One of the sockets has data to read
-			//	close(current_event.data.fd);
-			//}
-	//		pthread_mutex_unlock(ev_args->mutex);
 		}
-	}
-
-	if (events) {
-		free(events);
-	}
-	if (buffer) {
-		free(buffer);
 	}
 }
 
 bool clearSocket(int socket, char* buf, const int len) {
-	// something is going wrong in this function...
-	int bytesLeft = len;
 
-	readAllFromSocket(socket, buf, len);
-
-	bytesLeft = len;
+    int bytesLeft = len;
+	int nRead = readAllFromSocket(socket, buf, len);
+	printf("Read %d bytes from socket %d\n", nRead, socket);
+	printf("Buffer after recv: %s\n", buf);
 	if (!sendAll(socket, buf, &bytesLeft)) {
 		perror("sendall");
 		fprintf(stderr, "Only %d bytes because of the error\n", bytesLeft);
 	}
-	return true;
-	//while (true) {
-	////	bp = buf;
-	//	while (n < len) {
-	//		n = recv(socket, bp, bytesLeft, 0);
-	//		if (errno == EAGAIN || n == 0) { // need this for edge-triggered mode
-	//			return false;
-	//		}
-	//		bp += n;
-	//		bytesLeft -= n;
-	//	}
-	//	printf("sending: %s\n", buf);
-	//	n = send(socket, buf, len, 0);
-	//	if (n == -1) {
-	//		// client closed the connection
-	//		return false;
-	//	}
-	//	return true;
-	//}
 	//close(socket);
-	//return false;
+	return true;
 }
 
 void runEpoll(int listenSocket, pthread_t *workers, const int numWorkers, const short port, const int bufferLength)
 {
 	int status;
-	int server_fd;
-	int epoll_fd;
 	struct epoll_event event;
-	event_loop_args args;
+	event_loop_args* args = calloc(1, sizeof(event_loop_args));
 
-	server_fd = listenSocket;
+	args->server_fd = listenSocket;
 
-	setNonBlocking(server_fd);
+	setNonBlocking(args->server_fd);
 
-	epoll_fd = epoll_create1(0); // might need flags
-	if (epoll_fd == -1) {
+	args->epoll_fd = epoll_create1(0); // might need flags
+	if (args->epoll_fd == -1) {
 		perror("epoll_create1");
 		return;
 	}
 
 	// Register the server socket for epoll events
-	event.data.fd = server_fd;
+	event.data.fd = args->server_fd;
 	event.events = EPOLLIN | EPOLLET | EPOLLEXCLUSIVE;
-	status = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event);
+	status = epoll_ctl(args->epoll_fd, EPOLL_CTL_ADD, args->server_fd, &event);
 	if (status == -1) {
 		perror("epoll_ctl");
 		return;
 	}
 
 
-	args.epoll_fd = epoll_fd;
-	args.server_fd = server_fd;
-	args.bufLen = (size_t) bufferLength;
-	args.buffer = calloc(bufferLength, sizeof(char));
+	args->bufLen = (size_t) bufferLength;
+	args->buffer = calloc(bufferLength, sizeof(char));
+	args->events = calloc(MAX_EVENTS, sizeof(struct epoll_event));
 
 	//pthread_mutex_t* temp = NULL;
 	//pthread_mutex_init(temp, NULL);
@@ -221,7 +184,7 @@ void runEpoll(int listenSocket, pthread_t *workers, const int numWorkers, const 
 	// Start the event loop
     for (int i = 0; i < numWorkers; i++)
     {
-        pthread_create(workers + i, NULL, eventLoop, (void*) &args);
+        pthread_create(workers + i, NULL, eventLoop, (void*) args);
     }
 }
 
